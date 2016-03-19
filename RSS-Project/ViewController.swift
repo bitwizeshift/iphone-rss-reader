b7dfbed269b5888cec60b69c983d862645deca63
Matthew Rodusek <rodu4140@n2095-mac-18.local>
2016-03-19 14:38:36 -0400

    Initial Commit

---
+++ RSS-Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSS-Project
+//
+//  Created by Matthew Rodusek on 2016-03-19.
+//  Copyright © 2016 Wilfrid Laurier University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
