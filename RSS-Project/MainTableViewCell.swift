c44f15b20b52d4c212b6fb6bb51fba875380c6d1
Tomas Baena <baen9140@mylaurier.ca>
2016-03-22 18:03:33 -0400

    Add UI elements enabling left and right side menus

---
+++ RSS-Project/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  RSS-Project
+//
+//  Created by Tomas Baena on 2016-03-19.
+//  Copyright © 2016 Wilfrid Laurier University. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
